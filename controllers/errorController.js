29c02e362a40ece318f5c5b9d29b6c59ce515f76
Dexter1145 <divy.jain.19@gmail.com>
2023-12-20 22:39:35 +0530

    error handelling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,16 @@
-const sendErrorDev = (res, err) => {
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
@@ -6,16 +18,21 @@ const sendErrorDev = (res, err) => {
     stack: err.stack,
   });
 };
-const sendErrorProd = (res, err) => {
-  // Operational , Trusted error : send message to client
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
-    // programming or other unknown error : dont send to client dont nead to leak details
+
+    // Programming or other unknown error: don't leak error details
   } else {
-    console.error('ERROR 💥', err);
+    // 1) Log error
+    // console.error('ERROR 💥', err);
+
+    // 2) Send generic message
     res.status(500).json({
       status: 'error',
       message: 'Something went very wrong!',
@@ -24,12 +41,17 @@ const sendErrorProd = (res, err) => {
 };
 
 module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
+
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(err);
+    sendErrorProd(error, res);
   }
 };
